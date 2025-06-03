7aa5d4e315137998aaa9030a3e81505f27cbb7cf
Elizaveta Babenkova <liz2405@mail.ru>
2025-06-03 23:50:12 +0300

    dsssaa

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,9 @@
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied, ValidationError
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
-from django.db.models import Q
 from posts.models import Post, Comment, Follow, Group
 from .serializers import (
     PostSerializer,
@@ -12,6 +11,7 @@ from .serializers import (
     FollowSerializer,
     GroupSerializer
 )
+
 
 User = get_user_model()
 
@@ -28,18 +28,18 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
-        queryset = Post.objects.select_related('author', 'group').prefetch_related('comments')
-        
-        # Фильтрация по автору
+        queryset = Post.objects.select_related(
+            'author', 'group'
+        ).prefetch_related('comments')
+
         author_username = self.request.query_params.get('author')
         if author_username:
             queryset = queryset.filter(author__username=author_username)
-            
-        # Фильтрация по группе
+
         group_slug = self.request.query_params.get('group')
         if group_slug:
             queryset = queryset.filter(group__slug=group_slug)
-            
+
         return queryset.order_by('-pub_date')
 
     def perform_create(self, serializer):
@@ -59,7 +59,11 @@ class PostViewSet(viewsets.ModelViewSet):
     def comments(self, request, pk=None):
         post = self.get_object()
         comments = post.comments.all()
-        serializer = CommentSerializer(comments, many=True, context={'request': request})
+        serializer = CommentSerializer(
+            comments,
+            many=True,
+            context={'request': request}
+        )
         return Response(serializer.data)
 
 
@@ -77,7 +81,9 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_update(self, serializer):
         if serializer.instance.author != self.request.user:
-            raise PermissionDenied("Вы не можете редактировать чужой комментарий")
+            raise PermissionDenied(
+                "Вы не можете редактировать чужой комментарий"
+            )
         serializer.save()
 
     def perform_destroy(self, instance):
@@ -91,24 +97,30 @@ class FollowViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Follow.objects.filter(user=self.request.user).select_related('following')
+        return Follow.objects.filter(
+            user=self.request.user
+        ).select_related('following')
 
     def perform_create(self, serializer):
         following = serializer.validated_data['following']
         if self.request.user == following:
-            raise ValidationError({"following": "Вы не можете подписаться на самого себя"})
+            raise ValidationError(
+                {"following": "Вы не можете подписаться на самого себя"}
+            )
         serializer.save(user=self.request.user)
 
     @action(detail=False, methods=['get'])
     def followers(self, request):
-        """Получить список подписчиков текущего пользователя"""
-        followers = Follow.objects.filter(following=request.user).select_related('user')
+        """Получить список подписчиков текущего пользователя."""
+        followers = Follow.objects.filter(
+            following=request.user
+        ).select_related('user')
         serializer = self.get_serializer(followers, many=True)
         return Response(serializer.data)
 
     @action(detail=False, methods=['get'])
     def following(self, request):
-        """Получить список подписок текущего пользователя"""
+        """Получить список подписок текущего пользователя."""
         following = self.get_queryset()
         serializer = self.get_serializer(following, many=True)
         return Response(serializer.data)
